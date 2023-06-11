afe2b5cafa75cd089c2f02004e67157cba40bdfa
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-06-11 23:10:36 +0600

    edit

--- index.js
+++ index.js
@@ -86,3 +86,59 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`The Server is running on port ${port}`);
 });
+
+// app.post("/users", async (req, res) => {
+//   const user = req.body;
+//   const query = { email: user.email };
+//   const existingUser = await usersCollection.findOne(query);
+
+//   if (existingUser) {
+//     return res.send({ message: "user already exists" });
+//   }
+
+//   const result = await usersCollection.insertOne(user);
+//   res.send(result);
+// });
+// //get the user
+
+// app.get("/users", async (req, res) => {
+//   const result = await usersCollection.find().toArray();
+//   res.send(result);
+// });
+
+// app.get("/users/role/:id", async (req, res) => {
+//   const result = await usersCollection.find().toArray();
+//   res.send(result);
+// });
+
+// app.get("/users/role/:email", async (req, res) => {
+//   const email = req.params.email;
+
+//   if (req.decoded.email !== email) {
+//     res.send({ admin: false, instructor: false });
+//     return;
+//   }
+
+//   const query = { email: email };
+//   const user = await usersCollection.findOne(query);
+//   const result = {
+//     admin: user?.role === "admin",
+//     instructor: user?.role === "instructor",
+//   };
+//   res.send(result);
+// });
+
+// app.patch("/users/role/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const { role } = req.body;
+
+//   const filter = { _id: new ObjectId(id) };
+//   const updateDoc = {
+//     $set: {
+//       role: role === "instructor" ? "instructor" : "admin",
+//     },
+//   };
+
+//   const result = await usersCollection.updateOne(filter, updateDoc);
+//   res.send(result);
+// });
