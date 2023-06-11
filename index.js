2f190745c66cb63d5893d15369d841f79c086cfd
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-06-11 23:17:51 +0600

    path the data role

--- index.js
+++ index.js
@@ -58,19 +58,34 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/classes/admin/:id", async (req, res) => {
+    app.patch("/classes/role/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
+      const { role } = req.body;
+
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: "admin",
+          role: role === "instructor" ? "instructor" : "admin",
         },
       };
 
       const result = await classCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+
+    // app.patch("/classes/admin/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   console.log(id);
+    //   const filter = { _id: new ObjectId(id) };
+    //   const updateDoc = {
+    //     $set: {
+    //       role: "admin",
+    //     },
+    //   };
+
+    //   const result = await classCollection.updateOne(filter, updateDoc);
+    //   res.send(result);
+    // });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
